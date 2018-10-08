d541955f6a4b92fe838f6a987ff4c09ef73b2712
Luc FASQUELLE <mail@riges.net>
2018-10-08 14:54:02 +0200

    Energy - Set Room therm point

--- src/Netatmo/IEnergyClient.cs
+++ src/Netatmo/IEnergyClient.cs
@@ -10,5 +10,6 @@ namespace Netatmo
         Task<DataResponse<GetHomesDataBody>> GetHomesData(string homeId = null, string gatewayTypes = null);
         Task<DataResponse<GetHomeStatusBody>> GetHomeStatus(string homeId, string[] deviceTypes = null);
         Task<bool> SetThermMode(string homeId, string mode, LocalDateTime? endTime = null);
+        Task<bool> SetRoomThermPoint(string homeId, string roomId, string mode, double? temp = null, LocalDateTime? endTime = null);
     }
 }
---
+++ src/Netatmo/Models/Client/Energy/SetRoomThermpointRequest.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+using NodaTime;
+
+namespace Netatmo.Models.Client.Energy
+{
+    public class SetRoomThermpointRequest
+    {
+        [JsonProperty("home_id")]
+        public string HomeId { get; set; }
+
+        [JsonProperty("room_id")]
+        public string RoomId { get; set; }
+
+        [JsonProperty("mode")]
+        public string Mode { get; set; }
+
+        [JsonProperty("temp")]
+        public double? Temp { get; set; }
+
+        [JsonProperty("endtime")]
+        public LocalDateTime? EndTime { get; set; }
+    }
+}
