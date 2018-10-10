8e9dad89917fcae4a9b156e3210e31466267cfc0
Luc FASQUELLE <mail@riges.net>
2018-10-10 14:28:24 +0200

    Eneregy - Rename home schedule

--- src/Netatmo.Tests/EnergyClient.cs
+++ src/Netatmo.Tests/EnergyClient.cs
@@ -162,6 +162,30 @@ namespace Netatmo.Tests
         }
 
         [Fact]
+        public async Task RenameHomeSchedule_Should_Return_Expected_Result()
+        {
+            var homeId = "5a327cbdb05a2133678b5d3e";
+            var scheduleId = "5a327cbdb05a2133678b5d3f";
+            var name = "Cat schedule";
+            httpTest.RespondWithJson(new DataResponse
+            {
+                Status = "ok", TimeExec = 0.036107063293457,
+                TimeServer = new LocalDateTime(1970, 1, 1, 0, 0, 0).PlusSeconds(1518023467)
+            });
+
+            var sut = new Netatmo.EnergyClient("https://api.netatmo.com/", credentialManagerMock.Object);
+            await sut.RenameHomeSchedule(homeId, scheduleId, name);
+
+            httpTest
+                .ShouldHaveCalled("https://api.netatmo.com/api/renamehomeschedule")
+                .WithVerb(HttpMethod.Post)
+                .WithOAuthBearerToken(accessToken)
+                .WithContentType("application/json").WithRequestJson(new RenameHomeScheduleRequest
+                    {HomeId = homeId, ScheduleId = scheduleId, Name = name})
+                .Times(1);
+        }
+
+        [Fact]
         public async Task SetRoomThermPoint_Should_Return_Expected_Result()
         {
             var homeId = "5a327cbdb05a2133678b5d3e";
